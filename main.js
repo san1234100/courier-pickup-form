fef38229c6b13c88c239c55a7107b5ea8884d0cf
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-02-11 15:43:03 +0530

    form-validation

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
 import './style.css'
+import JustValidate from 'just-validate';
 
+const courierRequestFormEl = document.forms.courierRequestForm
+const validateForm = new JustValidate(courierRequestFormEl);
+// console.log(validateForm);
+// console.log(courierRequestFormEl);
 
+validateForm.addField('#fullname',[
+{
+    rule:'required'
+},
+{
+    rule:'minLength',
+    value: 3,
+},
+{
+    rule:'maxLength',
+    value: 20,
+}
+],
+{
+    errorFieldCssClass:["form-error"] //Adding tailwindcss classes to err msg
+})
+.addField('#phonenumber',[
+    {
+        rule:'required'
+    },
+    {
+        rule:'number'
+    },
+    {
+        rule:'minLength',
+        value: 10,
+    },
+    {
+        rule:'maxLength',
+        value: 10,
+    }
+    ],
+    {
+        errorFieldCssClass:["form-error"] //Adding tailwindcss classes to err msg
+    })
 
-
+// const formData = new FormData(courierRequestFormEl);
+// console.log([...formData]);
